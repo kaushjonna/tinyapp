a5ad5e65b3ec4f1836f800b46380b08a844c66dd
Kaush <kaush.jonna@gmail.com>
2019-02-13 09:17:46 -0500

    added some style changes

--- express_server.js
+++ express_server.js
@@ -38,6 +38,11 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortUrl}`);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
