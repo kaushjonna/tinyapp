427fd541129c7d72abbaf7a5ef498cb8c7bcf92c
Kaush <kaush.jonna@gmail.com>
2019-02-13 11:30:06 -0500

    added and tested the edit feature for individual links

--- express_server.js
+++ express_server.js
@@ -43,6 +43,11 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
